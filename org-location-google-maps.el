41f1f83e36c40432a0068c7aed3d3839a872b2d6
Julien Danjou <julien@danjou.info>
2010-06-28 15:55:05 +0200

    Import org-location-google-maps
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ org-location-google-maps.el
@@ -0,0 +1,56 @@
+;;; google-maps-static.el --- Access Google Maps Static from Emacs
+
+;; Copyright (C) 2010 Free Software Foundation, Inc.
+
+;; Author: Julien Danjou <julien@danjou.info>
+;; Keywords: comm
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Integrate google-maps into org-mode.
+;;
+;; This allows you to press C-c M-l on an Org entry to get the Google Map of
+;; your appointment.
+;;
+;;; Code:
+
+(require 'google-maps)
+(require 'org)
+(require 'org-agenda)
+
+(defun org-location-google-maps ()
+  "Show Google Map for location of an Org entry in an org buffer."
+  (interactive)
+  (let ((location (org-entry-get nil "LOCATION")))
+    (when location
+      (google-maps location))))
+
+(define-key org-mode-map "\C-c\M-l" 'jd:org-google-maps-location)
+
+(defun org-agenda-location-google-maps ()
+  "Show Google Map for location of an Org entry in an org-agenda buffer."
+  (interactive)
+  (let ((location
+         (save-window-excursion
+           (org-agenda-goto)
+           (org-entry-get nil "LOCATION"))))
+    (when location
+      (google-maps location))))
+
+(define-key org-agenda-mode-map "\C-c\M-l" 'jd:org-agenda-google-maps-location)
+
+(provide 'org-location-google-maps)
