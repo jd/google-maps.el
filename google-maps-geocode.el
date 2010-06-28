9b6954471b1132b43b11e9f4d0ff084908d8295d
Julien Danjou <julien@danjou.info>
2010-06-28 13:33:23 +0200

    Typo
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ google-maps-geocode.el
@@ -0,0 +1,102 @@
+;;; google-maps-geocode.el --- Encode address
+
+;; Copyright (C) 2010 Free Software Foundation, Inc.
+
+;; Author: Julien Danjou <julien@danjou.info>
+;; Keywords: comm
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; None
+;;; Code:
+(eval-when-compile
+  (require 'cl))
+
+(require 'json)
+(require 'google-maps)
+
+(defconst google-maps-geocode-uri
+  "http://maps.google.com/maps/api/geocode/json"
+  "Google Maps Geocoding API server.")
+
+(defun google-maps-geocode-build-url (plist)
+  "Built a Google Maps Geocode API request URL from PLIST."
+  (concat
+   google-maps-geocode-uri "?"
+   (google-maps-urlencode-plist
+    plist
+    '((address . url-hexify-string)
+      ;; Sensor is MANDATORY
+      (sensor . (lambda (v)
+                  (if v "true" "false")))
+      (latlng)
+      (region)
+      (bounds . (lambda (v)
+                  (mapconcat 'identity v "|")))
+      (language)))))
+
+(defun google-maps-geocode-request->status (data)
+  "Return status of a `google-maps-geocode' request.
+It should returned `ok' if everything went well."
+  (intern (downcase (cdr (assoc 'status data)))))
+
+(defun google-maps-geocode-request-is-ok (data)
+  "Return t if the geocoding via `google-maps-geocode' went fine."
+  (eq (google-maps-geocode-status data) 'ok))
+
+(defun google-maps-geocode-request->results (data)
+  "Return result list of a `google-maps-geocode' request."
+  (cdr (assoc 'results data)))
+
+(defun google-maps-geocode-request (&rest params)
+  "Request geocoding of a location and return the request result.
+Request status can be retrieved with
+`google-maps-geocode-request->status'. Request results data can
+be retrieve ed with `google-maps-geocode-request->results'."
+  (json-read-from-string
+   (google-maps-retrieve-data
+    (google-maps-geocode-build-url
+     (google-maps-build-plist params)))))
+
+(defun google-maps-geocode-results->one-result-picked-by-user (results)
+  "Make the user choose a result from RESULTS, and return it."
+  (let ((location
+         ;; `location' will contains what the user choosed as location.
+         (completing-read
+          "Precise location: "
+          (mapcar
+           (lambda (x)
+             (cdr (assoc 'formatted_address x)))
+           results)
+          nil t)))
+    ;; Find entry with that location
+    (find-if
+     `(lambda (entry)
+        (string= (cdr (assoc 'formatted_address entry))
+                 ,location))
+     results)))
+
+(defun google-maps-geocode-results->one-result (results)
+  "Converts geocoding results list to one result only.
+If there is several results, the user is asked to pick one via
+`google-maps-geocode-results->one-result-picked-by-user'."
+  (case (length results)
+    (0 nil)
+    (1 (elt results 0))
+    (t (google-maps-geocode-results->one-result-picked-by-user results))))
+
+(provide 'google-maps-geocode)
